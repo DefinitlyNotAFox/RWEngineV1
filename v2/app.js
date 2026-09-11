a35ff96db3d4399b82f2300dfcfa089c8c9b5288
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-11 14:21:21 +0200

    Refresh faction aggregate header module

--- v2/app.js
+++ v2/app.js
@@ -8,7 +8,7 @@ import {
 } from './core.js';
 
 import { initIntel, renderIntel, refreshSyncStatus } from './intel.js?v=2';
-import { initIntelV2 } from './intel-v2.js?v=11';
+import { initIntelV2 } from './intel-v2.js?v=12';
 import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=2';
 
 const legacyIntelMode = new URL(location.href).searchParams.get('legacyIntel') === '1';
