d3fd5b378c6dc019652e97cfd9f24410ea79b97b
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-11 14:47:45 +0200

    Refresh dedicated aggregate module

--- v2/app.js
+++ v2/app.js
@@ -8,7 +8,7 @@ import {
 } from './core.js';
 
 import { initIntel, renderIntel, refreshSyncStatus } from './intel.js?v=2';
-import { initIntelV2 } from './intel-v2.js?v=15';
+import { initIntelV2 } from './intel-v2.js?v=16';
 import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=2';
 
 const legacyIntelMode = new URL(location.href).searchParams.get('legacyIntel') === '1';
