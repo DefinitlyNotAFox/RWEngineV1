7e1c58f268dc5cf53f85db9d72a94c4d740162c8
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-11 14:32:15 +0200

    Refresh faction filter toggles

--- v2/app.js
+++ v2/app.js
@@ -8,7 +8,7 @@ import {
 } from './core.js';
 
 import { initIntel, renderIntel, refreshSyncStatus } from './intel.js?v=2';
-import { initIntelV2 } from './intel-v2.js?v=13';
+import { initIntelV2 } from './intel-v2.js?v=14';
 import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=2';
 
 const legacyIntelMode = new URL(location.href).searchParams.get('legacyIntel') === '1';
