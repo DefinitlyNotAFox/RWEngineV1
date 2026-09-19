2f649b306770b9b52f5fa45940c533f50dcf9c39
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-19 23:57:48 +0200

    Load updated core settings API

--- v2/wars.js
+++ v2/wars.js
@@ -5,7 +5,7 @@ import {
   canEditFactionView, renderLeadershipMarker,
   metric, formatNumber, formatDecimal, formatSigned, formatPercent,
   formatDate, escapeHtml, sleep, warStamp
-} from './core.js?v=3';
+} from './core.js?v=4';
 
 const ATTACK_STEP_DELAY = 6000;
 const DETAIL_STEP_DELAY = 1200;
