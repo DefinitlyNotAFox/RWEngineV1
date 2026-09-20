c7f3c0f35efb21abae34ea17584c84655e94ad3f
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-20 18:14:03 +0200

    Refresh faction Intel module

--- v2/app.js
+++ v2/app.js
@@ -13,7 +13,7 @@ import {
 import { sortFactionAccounts, sortTrackedFactions } from './sort.js?v=1';
 
 import { initIntel, renderIntel, refreshSyncStatus } from './intel.js?v=6';
-import { initIntelV2 } from './intel-v2.js?v=54';
+import { initIntelV2 } from './intel-v2.js?v=55';
 import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=23';
 
 const legacyIntelMode = new URL(location.href).searchParams.get('legacyIntel') === '1';
