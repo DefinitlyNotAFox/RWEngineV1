f76e400fd7c5a41fbccbb56a56089ebf730107bd
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-20 13:14:21 +0200

    Load active tag filter options

--- v2/app.js
+++ v2/app.js
@@ -13,7 +13,7 @@ import {
 import { sortFactionAccounts, sortTrackedFactions } from './sort.js?v=1';
 
 import { initIntel, renderIntel, refreshSyncStatus } from './intel.js?v=6';
-import { initIntelV2 } from './intel-v2.js?v=51';
+import { initIntelV2 } from './intel-v2.js?v=52';
 import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=19';
 
 const legacyIntelMode = new URL(location.href).searchParams.get('legacyIntel') === '1';
