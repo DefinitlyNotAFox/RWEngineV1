d37efbf6bf9f343c27784968eae56e8d92627aed
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-20 17:47:01 +0200

    Bump payout war module asset version

--- v2/app.js
+++ v2/app.js
@@ -14,7 +14,7 @@ import { sortFactionAccounts, sortTrackedFactions } from './sort.js?v=1';
 
 import { initIntel, renderIntel, refreshSyncStatus } from './intel.js?v=6';
 import { initIntelV2 } from './intel-v2.js?v=54';
-import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=22';
+import { initWarViews, renderWarOverview, renderArchive } from './wars.js?v=23';
 
 const legacyIntelMode = new URL(location.href).searchParams.get('legacyIntel') === '1';
 
