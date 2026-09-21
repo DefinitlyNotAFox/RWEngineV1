686f8b51e5b53c5b95f856cfb4d6f4efd701a46d
DefinitlyNotAFox <realfoxiboxi@gmail.com>
2026-09-22 00:20:51 +0200

    Refresh shared core module

--- v2/intel-v2.js
+++ v2/intel-v2.js
@@ -3,7 +3,7 @@ import {
   canEditFactionView, renderLeadershipMarker,
   formatNumber, formatCompact, formatDecimal, formatPercent, formatSigned,
   formatDuration, formatRelative, escapeHtml, sleep
-} from './core.js?v=8';
+} from './core.js?v=9';
 import { buildAutoTags } from './auto-tag-engine.js?v=4';
 
 const MONTH_DAYS = 30.44;
